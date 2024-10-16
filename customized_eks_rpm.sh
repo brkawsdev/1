562054be4e73628810eab70f6319bf1dc928354c
brkawsdev <87372939+brkawsdev@users.noreply.github.com>
2024-10-16 14:16:24 +0530

    Update customized_eks_rpm.sh
    Edited update command

--- customized_eks_rpm.sh
+++ customized_eks_rpm.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo yum udpate -y
+sudo yum update -y
 terraform_version=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
 curl -O "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip"
 unzip terraform_${terraform_version}_linux_amd64.zip && mkdir -p ~/bin && mv terraform ~/bin/ && terraform version
